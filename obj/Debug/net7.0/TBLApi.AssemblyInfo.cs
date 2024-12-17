83486310738c8d022b0416a2f08326552bcf7e91
LDE22 <144797842+LDE22@users.noreply.github.com>
2024-12-17 21:40:07 +0300

    +

--- obj/Debug/net7.0/TBLApi.AssemblyInfo.cs
+++ obj/Debug/net7.0/TBLApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TBLApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb2222e7e9c4b6e23b6e7cb5f57975c1f64a3c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c671bbc12a5de28751079b3ddbe4004e80440e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TBLApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TBLApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
