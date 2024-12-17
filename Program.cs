59b6ca8294e8af71851afcc29bd83f46cfb98724
LDE22 <144797842+LDE22@users.noreply.github.com>
2024-12-18 00:10:21 +0300

    +

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using TBLApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
+// Добавляем поддержку контроллеров
+builder.Services.AddControllers();
+
+// Добавляем Swagger для документирования API
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Настройка строки подключения к PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Добавляем CORS для поддержки внешних клиентов
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
     {
-        options.JsonSerializerOptions.PropertyNamingPolicy = null; // Отключаем camelCase (по желанию)
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
+});
 
 var app = builder.Build();
 
+// Настройка Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
 
 app.MapControllers();
--- obj/Debug/net7.0/TBLApi.AssemblyInfo.cs
+++ obj/Debug/net7.0/TBLApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TBLApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9850ee8433bbed5049f9494743c1aa4f8cdfa472")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9acca7c61a8bf0d319e4bee5623b0ddabe40eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TBLApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TBLApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
